93344a967dc9e593f962832a35af0b05666cfdbc
AdamRWeinstein <adam.rick.weinstein@gmail.com>
2024-02-05 10:15:08 -0500

    Add folders and initial Model

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
+// Imports
 require('dotenv').config();
+require('./config/db.connection')
 const { PORT } = process.env;
 const express = require('express');
+const cors = require("cors");
+const morgan = require("morgan");
+
 const app = express();
 
+const ridesRouter = require('./routes/rides')
+
+app.use(express.urlencoded({extended:true}))
+app.use(express.json());
+
+app.use(cors());
+app.use(morgan("dev"));
+
+app.use('/rides', ridesRouter)
+
+// Routes
 app.get("/", (req, res) => {
     res.send("hello world")
 })
